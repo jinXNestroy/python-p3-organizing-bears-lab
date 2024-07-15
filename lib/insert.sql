7a6b516a4d3e02203b8d88c6b40a8faf02d49d1e
DanielJoel <danpersie53@gmail.com>
2024-07-15 22:41:46 +0300

    org bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(1, 'Mr. Chocolate', 12, 'M', 'black', 'gucci', True);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(2, 'Rowdy', 8, 'M', 'brown', 'aggressive', True);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(3, 'Tabitha', 5, 'F', 'black', 'playful', True);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(4, 'Sergeant Brown', 10, 'M', 'brown', 'protective', True);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(5, 'Melissa', 7, 'F', 'brown', 'curious', True);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(6, 'Grinch', 6, 'M', 'black', 'grumpy', True);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(7, 'Wendy', 4, 'F', 'black', 'gentle', True);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(8, NULL, 3, 'M', 'brown', 'unknown', True);
